cfa8821cc2647c990cc34df6292d1264cb0c9a8d
Nathan Mentley <nathan@Nathans-MacBook-Air.local>
2023-02-16 18:06:10 -0500

    Updating to the new repo to the new protocol

--- Sources/JazzDataAccessSqlite/SqliteRepository.swift
+++ Sources/JazzDataAccessSqlite/SqliteRepository.swift
@@ -13,26 +13,19 @@ public final class SqliteRepository<TResource: Storable>: Repository<TResource> 
         super.init();
     }
 
-    public final override func open() async throws {
+    public final override func open() async throws {}
+
+    public final override func close() async throws {}
+
+    public final override func create(_ models: [TResource], with hints: [QueryHint]) async throws -> [TResource] {
+        return models;
     }
 
-    public final override func close() async throws {
+    public final override func update(_ models: [TResource], with hints: [QueryHint]) async throws -> [TResource] {
+        return models;
     }
 
-    public final override func create(_ model: TResource, with hints: [QueryHint]) async throws -> TResource {
-        return model;
-    }
-
-    public final override func delete(id: String, with hints: [QueryHint]) async throws {
-    }
-
-    public final override func update(_ model: TResource, with hints: [QueryHint]) async throws -> TResource {
-        return model;
-    }
-
-    public final override func get(id: String, with hints: [QueryHint]) async throws -> TResource {
-        throw DataAccessErrors.notFound(reason: "Could not find resource for \(id).");
-    }
+    public final override func delete(for criteria: [QueryCriterion], with hints: [QueryHint]) async throws {}
 
     public final override func get(for criteria: [QueryCriterion], with hints: [QueryHint]) async throws -> [TResource] {
         return [];
